1465cbb68a4a6af90543bceb1d8ad09bd54f1123
knaaptime <knaaptime@users.noreply.github.com>
2026-02-07 19:23:14 +0000

    deploy: f38a681b368159e258bd28e05a0eda9e365b0cbf

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.5.dev36+gcaa4c33c0',
+    VERSION: '0.0.5.dev40+gf38a681b3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
