dab43ebfb4c12f101b549fa8573dd0a44a028374
jGaboardi <jGaboardi@users.noreply.github.com>
2026-02-22 00:05:27 +0000

    deploy: d057a76eff4205522f2cf743964cfde1abd7b602

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.5.dev40+gf38a681b3',
+    VERSION: '0.0.5.dev55+gd057a76ef',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
